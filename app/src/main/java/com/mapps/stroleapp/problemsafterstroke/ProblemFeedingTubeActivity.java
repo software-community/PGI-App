ea969c3202223ba7031c60006ffdad16a379d40c
Sachin Bijalwan <2015csb1027@iitrpr.ac.in>
2018-02-14 16:51:41 +0000

    ui finalized

--- app/src/main/java/com/mapps/stroleapp/problemsafterstroke/ProblemFeedingTubeActivity.java
+++ app/src/main/java/com/mapps/stroleapp/problemsafterstroke/ProblemFeedingTubeActivity.java
@@ -1,10 +1,13 @@
 package com.mapps.stroleapp.problemsafterstroke;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +18,10 @@ public class ProblemFeedingTubeActivity extends AppCompatActivity {
     private Button pickTimeFtlc, submitFeedingTubeInfo, feedingTubeBack;
     private EditText feedingTubeDurationInDays;
     private TextView ftlcTime;
+    private int mYear, mMonth, mDay;
+    private int[] date;
+    private boolean d;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +39,30 @@ public class ProblemFeedingTubeActivity extends AppCompatActivity {
             }
         }
         );
+    }
+    public void onClick(View v) {
+        Button temp = (Button) v;
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        temp.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                        date = new int[]{dayOfMonth, monthOfYear + 1, year};
+                        d = true;
+
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
     }
 }
